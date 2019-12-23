fd87bf77b1a5c4d831a35061b5030f679ce5fee7
Cristian Torres <c.torres@mdxsolutions.co.uk>
2019-12-23 10:29:50 +0000

    Adding User Transactions to the List

--- lib/widgets/user_transactions.dart
+++ lib/widgets/user_transactions.dart
@@ -31,7 +31,7 @@ class _UserTransactionsState extends State<UserTransactions> {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        NewTransaction(),
+        NewTransaction(_addNewTransaction),
         TransactionList(_userTransactions),
       ],
     );
